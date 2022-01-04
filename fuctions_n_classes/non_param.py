467b56d8cd49c6c6abd0ed312660ad2f1bd398fb
Kuldeepak <>
2022-01-04 18:30:43 +0530

    Create non_param.py

---
+++ fuctions_n_classes/non_param.py
@@ -0,0 +1,10 @@
+def check_prime():
+    a = 541
+    for i in range(2,a):
+        if a%i==0:
+            print("not a prime")
+            break
+    else:
+         print("prime")   
+if __name__ == "__main__" :
+    check_prime()                      
