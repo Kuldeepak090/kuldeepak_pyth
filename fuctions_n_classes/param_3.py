75537b4e19c3cbcfee0d90e6373c49381ebbfa5b
Kuldeepak <>
2022-01-04 18:30:53 +0530

    Create param_3.py

---
+++ fuctions_n_classes/param_3.py
@@ -0,0 +1,11 @@
+def word_counter(msg,limit):
+    words = msg.split()
+    wl = []
+    for items in words:
+        if len(items) == limit:
+            wl.append(items)
+    return len(wl)
+
+data = '''the author who
+ has a made '''  
+print("4 letter word count is",word_counter(data,limit =10))           
