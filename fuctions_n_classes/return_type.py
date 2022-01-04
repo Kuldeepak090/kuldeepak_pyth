9e963444f5940bc27a21631f1c62345c02b8e44e
Kuldeepak <>
2022-01-04 18:30:57 +0530

    Create return_type.py

---
+++ fuctions_n_classes/return_type.py
@@ -0,0 +1,11 @@
+def check_prime_2():
+    a= 495
+    for i in range(2,a):
+        if a%i ==0:
+            return "non prime"
+    else:
+        return "prime" 
+
+result=check_prime_2()   
+print("value is",result)      
+print("value",check_prime_2())      
