8f8006fc4904a4cd0db1cc9a43e56d8723192c5d
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:33 -0500

    Create f_1.py

---
+++ home work/function_homework/f_1.py
@@ -0,0 +1,9 @@
+### print the each word in different line of string python is everywhere
+
+def names(message):
+    return len(message.split())
+
+data = "python is everthing"
+print(names(data))
+
+
