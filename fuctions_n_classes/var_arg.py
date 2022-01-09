e929c219d8f65eb4b22697138377d7272653d7db
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:35 -0500

    Update var_arg.py

--- fuctions_n_classes/var_arg.py
+++ fuctions_n_classes/var_arg.py
@@ -1,17 +1,23 @@
 def avg(*values):
     return sum(values)/ len(values)
 
-def voewl_counter(vowel_counter, *words):
+def vowel_counter(vowel_char, *words):
     c =0
     for word in words:
-        vc= word.casefold.count(vowel_char)
+        vc= word.casefold().count(vowel_char)
         c +=vc
     return c
-# com 
-   
 
-count = voewl_counter("a", "apple","banana")    
-print(" a found", count, "times")
+   
+# comprehension
+
+def vowel_counter_comp(vowel_char , *words):
+    return sum([word.casefold().count(vowel_char) for word in words])
 
  #print(avg(1,2,3,3,123,13,12,33)) 
 #print(avg(1,12,3))   
+
+count = vowel_counter('a','apple','banana','cherry','japan')
+print("A found", count , "times")
+count = vowel_counter_comp('e','apple','banana','cherry','japan')
+print("E found",count,"times")
