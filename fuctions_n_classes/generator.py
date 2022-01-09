82def57b1f4d1e74c76406ed85e6de858cf5fbe3
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:52 -0500

    Update generator.py

--- fuctions_n_classes/generator.py
+++ fuctions_n_classes/generator.py
@@ -3,7 +3,7 @@ def get_cubes_old(*arg):
     for i in arg:
         x.append(i**3)
     return x
-print(get_cubes_old(1,2,3,4,5,))    
+print(get_cubes_old(1,2,3,3,3,3,31,23,132,1,4,5,))    
 
 
 
@@ -11,5 +11,5 @@ def get_cube(*arg):
     for i in arg:
         yield i**3
 
-for val in get_cube(23,44):
+for val in get_cube(11,23,44):
     print(val)
