32de3a3e9380909a4da5cecc39db884ed11440ff
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:29 -0500

    Delete homework.py

--- home work/homework.py
+++
@@ -1,9 +0,0 @@
-### print the each word in different line of string python is everywhere
-
-def names(message):
-    return len(message.split())
-
-data = "python is everthing"
-print(names(data))
-
-
