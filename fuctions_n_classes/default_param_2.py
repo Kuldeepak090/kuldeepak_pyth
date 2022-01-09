c08297121da0879fb3cfa4f272b68e239012b7e6
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:43 -0500

    Create default_param_2.py

---
+++ fuctions_n_classes/default_param_2.py
@@ -0,0 +1,12 @@
+def summer(a,b=0,c=0,d=0):
+    return a+b+c+d
+
+
+    #usage
+    print(summer(10))
+    print(summer(10, b=5))
+    print(summer(10,d =25))
+    print(summer(10,b=5,d=25))
+    print(summer(10, b=5, c=5))
+    print(summer(10,b=5,c=5,d=3))
+    print(summer(10,20,30,50))
