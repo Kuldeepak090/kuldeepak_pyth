30b9fe44a1fef7ef40f9e11091b3f9155be2abbe
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-09 02:04:41 -0500

    Update param.py

--- fuctions_n_classes/param.py
+++ fuctions_n_classes/param.py
@@ -1,3 +1,4 @@
+# check prime 3 accepts one parameter
 def check_prime_3(n):
     for i in range(2,n):
         if n% i ==0:
@@ -5,12 +6,17 @@ def check_prime_3(n):
         else:
             return "prime" 
 
-print("15 is", check_prime_3(15))               
+print("15 is", check_prime_3(15)) 
+print("17 is", check_prime_3(17)) 
+print("131231 is" , check_prime_3(131231))  
+
+#now we can use loop with functions
 
 x =[12,34,567,9]
 for i in x:
     print(f'{i} is {check_prime_3(i)}')
 
+# use with user input 
 
 num = int(input("enter the number:"))
 print(f" {num} :{check_prime_3(num)}")   
