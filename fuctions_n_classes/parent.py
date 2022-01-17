9dbbf1b1c6de83e6dc75bc278066c1aaab0bca3c
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-01-17 02:14:47 -0500

    Update parent.py

--- fuctions_n_classes/parent.py
+++ fuctions_n_classes/parent.py
@@ -9,10 +9,10 @@ class fruit:
 
     def show(self):
         print('name',self.name)   
-        print('shape',self.shape) 
+        print('size',self.size) 
         print('flavour',self.flavour)
+        print('shape',self.shape)
         print('color',self.color)
-        print('size',self.size)
 
 if __name__ == "__main__":
     a1 = fruit('apple','small','sweet', 'round')  
