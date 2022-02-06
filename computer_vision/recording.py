8fa3a5d320a73d725237a21e48e67f69de152d42
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-02-06 16:58:21 +0530

    Update recording.py

--- computer_vision/recording.py
+++ computer_vision/recording.py
@@ -1,22 +1,20 @@
+import cv2
 from cv2 import VideoCapture
-import numpy as np
-from cv2 import VideoCapture
+from cv2 import VideoWriter
 
 cap = VideoCapture(0)
 
-fourcc =cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc,20.0(640,480))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 60.0, (640,  480))
 
 while True:
     status,frame = cap.read()
     if not status:
         break
-    
     cv2.imshow('window',frame)
     out.write(frame)
-    if cv2.waitkey(1) == 27:
+    if cv2.waitKey(1) == 27:
         break
-
-out.realse()
-cap.realse()
-cv2.destroyAllWindoes()   
+out.release()
+cap.release()
+cv2.destroyAllWindows()  
