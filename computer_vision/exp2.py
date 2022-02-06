fb423bfb6c004f06fe5d56d3c9bf5d1ff50583de
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-02-06 16:58:15 +0530

    Update exp2.py

--- computer_vision/exp2.py
+++ computer_vision/exp2.py
@@ -7,4 +7,9 @@ while True:
         fast = cv2.FastFeatureDetector_create()
         kp = fast.detect(img,None)
         img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,255))
+        cv2.imshow('window', img2)
+        if cv2.waitKey(1) == 27:
+            break
+cap.release()
+cv2.destroyAllWindows()
         
